55a509176ff5f1d6fb224874cfe9b5c7464ffd82
yuki <abbq100@163.com>
2017-11-07 21:19:48 -0600

    add comment section for dishdetails

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,7 +5,8 @@ import { Location } from '@angular/common';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 import 'rxjs/add/operator/switchmap';
 
 @Component({
@@ -19,10 +20,33 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+  comment: Comment;
+  commentForm: FormGroup;
+  formErrors = {
+    'author': '',
+    'rating': '',
+    'comment': ''
+  };
+  validationMessages = {
+    'author': {
+      'required': 'Author Name is required.',
+      'minlength': 'Author Name must be at least 2 characters long.',
+      'maxlength': 'Author Name cannot be more than 25 characters long.'
+    },
+    'rating': {
+      'required': 'Rating is required.'
+    },
+    'comment': {
+      'required': 'Comment is required.'
+    }
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+      this.createForm();
+  }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -38,6 +62,55 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm(){
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      rating: [5, [Validators.required]],
+      comment: ['', [Validators.required]]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChange(data));
+
+    this.onValueChange(); // reset form validation message
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    console.log(this.comment);
+
+    // push new comment to dish comment array
+    let date = new Date();
+    this.dish.comments.push({
+      rating: this.comment.rating,
+      comment: this.comment.comment,
+      author: this.comment.author,
+      date: date.toISOString()
+    });
+
+    // reset comment form
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+  }
+
+  onValueChange(data?: any) {  // optional parameter
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
 }
