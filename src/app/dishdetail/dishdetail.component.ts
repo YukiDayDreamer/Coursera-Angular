516e1d54b4bb615bbbf13a41710945ca4edaf8a6
yuki <abbq100@163.com>
2017-12-15 21:50:57 -0500

    Add Inject baseUrl

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -44,11 +44,12 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
-    private fb: FormBuilder) {
-      this.createForm();
+    private fb: FormBuilder,
+    @Inject('BaseURL') private BaseURL) {
   }
 
   ngOnInit() {
+    this.createForm();
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.switchMap((params: Params) => this.dishservice.getDish(+params['id']))
     .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id)});
